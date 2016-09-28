345c5f5adeb31dc9acc4f08c1487cc7ed2bd96da
Steven Burris <stevenburris.jr@gmail.com>
2016-09-28 10:32:47 -0400

    linked some fields and stuff

---
+++ app/src/main/java/com/theironyard/todoandroid/ToDoItem.java
@@ -0,0 +1,15 @@
+package com.theironyard.todoandroid;
+
+/**
+ * Created by stevenburris on 9/28/16.
+ */
+
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
